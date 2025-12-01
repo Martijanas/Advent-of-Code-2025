ae3ee36e081b1efc434fecce3e0d78174731bcae
Martijanas <martynasjankus72@gmail.com>
2025-12-02 00:48:40 +0100

    Adding the 12-01 challange part 2

--- 12-01/Part 2/main.py
+++ 12-01/Part 2/main.py
@@ -18,15 +18,25 @@ try:
                 if int(stripped_line[1:]) < 100:
 
                     diff = abs(int(stripped_line[1:]) - start)
-                    start = start - int(stripped_line[1:])
-
-                    if start < 0:
-                        start = 100 - diff
 
                     if start == 0:
-                        password = password + 1
+                        start = 100 - int(stripped_line[1:])
+                        print("Dial point is at: ", start)
+                        print(password)
+                        continue
+
+                    start = start - int(stripped_line[1:])
+
+                    if start < 1:
+                        if diff == 0:
+                            start = 0
+                            password = password + 1
+                        else:
+                            start = 100 - diff
+                            password = password + 1
 
                     print("Dial point is at: ", start)
+                    print(password)
 
                 elif int(stripped_line[1:]) > 99:
 
@@ -35,15 +45,29 @@ try:
                     new_number = int(stripped_line[1:]) - x*100
 
                     diff = abs(new_number - start)
+
+                    if start == 0:
+                        start = 100 - new_number
+                        password = password + x
+                        print("Dial point is at: ", start)
+                        print(password)
+                        continue
+
                     start = start - new_number
 
                     if start < 0:
                         start = 100 - diff
 
+                        if start != 0:
+                            password = password + 1
+
                     if start == 0:
                         password = password + 1
 
+                    password = password + x
+
                     print("Dial point is at: ", start)
+                    print(password)
 
             elif stripped_line.startswith('R'):
 
@@ -54,11 +78,10 @@ try:
 
                     if start > 99:
                         start = 0 + diff
-
-                    if start == 0:
                         password = password + 1
 
                     print("Dial point is at: ", start)
+                    print(password)
 
                 elif int(stripped_line[1:]) > 99:
 
@@ -72,10 +95,16 @@ try:
                     if start > 99:
                         start = 0 + diff
 
+                        if start != 0:
+                            password = password + 1
+
                     if start == 0:
                         password = password + 1
 
+                    password = password + x
+
                     print("Dial point is at: ", start)
+                    print(password)
 
         print(password)
 
